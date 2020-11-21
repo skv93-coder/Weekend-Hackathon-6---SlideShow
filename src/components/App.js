0b80c6fd19108e869672e0a36b49caf671e32693
skv93-coder <68867418+skv93-coder@users.noreply.github.com>
2020-11-21 20:54:34 +0530

    Ch

--- src/components/App.js
+++ src/components/App.js
@@ -11,9 +11,9 @@ const App = (props) => {
     <>
     <h1 data-testid="title">{slides[idx].title}</h1>
     <p data-testid="text">{slides[idx].text}</p>
-    <button data-testid="button-restart" disabled={idx===0?true:false} onClick={()=>setidx(0)}></button>
-    <button data-testid="button-next" disabled={idx+1===slides.length?true:false} onClick={()=>setidx(idx+1)}></button>
-    <button data-testid="button-prev" disabled={idx===0?true:false} onClick={()=>setidx(idx-1)}></button>
+    <button data-testid="button-restart" disabled={idx===0?true:false} onClick={()=>setidx(0)}>Restart</button>
+    <button data-testid="button-next" disabled={idx+1===slides.length?true:false} onClick={()=>setidx(idx+1)}>Next</button>
+    <button data-testid="button-prev" disabled={idx===0?true:false} onClick={()=>setidx(idx-1)}>Prev</button>
 
 
     </>
