5bb3c03dab4af5e40664240a8473462c3f47772c
skv93-coder <68867418+skv93-coder@users.noreply.github.com>
2020-11-21 19:06:48 +0530

    Changes

--- src/components/Slides.jsx
+++ src/components/Slides.jsx
@@ -3,8 +3,11 @@ import slides from "../data.js";
 export default function   Slides(props){
   const {move,handle,handle1,h}=props;
   return (<>
-<div>{
-slides[move-1].text}</div>
+  <h1 data-testid="title">
+  {slides[move-1].title}
+  </h1>
+<p data-testid="text">{
+slides[move-1].text}</p>
 <button data-testid="button-next" onClick={handle}></button>
 <button data-testid="button-prev" onClick={handle1}></button>
 <button data-testid="button-restart" onClick={h}></button>
